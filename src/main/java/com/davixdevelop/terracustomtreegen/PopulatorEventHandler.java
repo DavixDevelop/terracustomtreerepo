914aed5cb0107da7a80cfba85b9303c5224705d0
David Rupnik <davidrupnik23@gmail.com>
2021-08-01 01:33:29 +0200

    Removed builtin datasets and switched to a common library mod

--- src/main/java/com/davixdevelop/terracustomtreegen/PopulatorEventHandler.java
+++ src/main/java/com/davixdevelop/terracustomtreegen/PopulatorEventHandler.java
@@ -2,10 +2,6 @@ package com.davixdevelop.terracustomtreegen;
 
 import com.davixdevelop.terracustomtreegen.baker.SegmentsBaker;
 import com.davixdevelop.terracustomtreegen.baker.TreeMapBaker;
-import com.davixdevelop.terracustomtreegen.dataset.ContinentsDataset;
-import com.davixdevelop.terracustomtreegen.dataset.ContinentsDataset;
-import com.davixdevelop.terracustomtreegen.dataset.KoppenClimate;
-import net.buildtheearth.terraplusplus.event.InitDatasetsEvent;
 import net.buildtheearth.terraplusplus.event.InitEarthRegistryEvent;
 import net.buildtheearth.terraplusplus.generator.data.IEarthDataBaker;
 import net.buildtheearth.terraplusplus.generator.populate.IEarthPopulator;
@@ -15,28 +11,15 @@ public class PopulatorEventHandler {
 
 	public static final String KEY_KOPPEN = "KEY_DAVIXDEVELOP_KOPPEN_DATASET";
 	public static final String KEY_CONTINENT = "KEY_DAVIXDEVELOP_CONTINETS_DATASET";
-	
+
 	@SubscribeEvent
 	public void populate(InitEarthRegistryEvent<IEarthPopulator> event) {
 		event.registry().set("trees", new CustomTreePopulator());
 	}
-	
+
 	@SubscribeEvent
 	public void baker(InitEarthRegistryEvent<IEarthDataBaker> event) {
 		event.registry().addLast("davixdevelop_linesegments", new SegmentsBaker());
 		event.registry().addLast("davixdevelop_treemap", new TreeMapBaker());
-	}
-
-	@SubscribeEvent
-	public void datasets(InitDatasetsEvent event){
-		boolean addKoppen = true;
-		try{
-			if(event.get(KEY_KOPPEN) != null)
-				addKoppen = false;
-		}catch (Exception ex){ }
-		if(addKoppen)
-			event.register(KEY_KOPPEN, new KoppenClimate());
-
-		event.register(KEY_CONTINENT, new ContinentsDataset());
 	}
 }
